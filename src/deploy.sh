ecc21f8bfad4d845c522df691ce65ee3aa8f7f95
jangh <jangh@211.105.0.214>
2022-11-09 21:53:12 +0900

    update

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-cd /home/devops/aap-iaas-cicd && git pull
-cp /home/devops/aap-iaas-cicd/src/html/index.html /tmp/index.html.tmp
-sudo rm -rf /var/www/html/index.html
-sudo cp /home/devops/aap-iaas-cicd/src/html/index.html /var/www/html/index.html
-sudo sed -i "s/__HOSTNAME__/$(hostname)/g" /var/www/html/index.html
+# Variables
+CICD_PATH="/home/devops/aap-iaas-cicd"
+SRC_PATH="$CICD_PATH/src"
+CONF_FILE="$SRC_PATH/deploy.conf"
+ORG_FILE="$SRC_PATH/html/index.html"
+TMP_FILE="/tmp/index.html.tmp"
+WWW_FILE="/var/www/html/index.html"
+
+# Update Source Code && Load Config Variable
+cd $CICD_PATH && git pull
+source $CONF_FILE
+
+# Build New "index.html" via Config Variable
+sudo cp $ORG_FILE $TMP_FILE
+sudo sed -i "s/__HEAD_TITLE__/$HEAD_TITLE/g" $TMP_FILE
+sudo sed -i "s/__BODY_MARKUP__/$BODY_MARKUP/g" $TMP_FILE
+sudo sed -i "s/__BODY_VERSION__/$BODY_VERSION/g" $TMP_FILE
+sudo sed -i "s/__HOSTNAME__/$(hostname)/g" $TMP_FILE
+sudo mv $TMP_FILE $WWW_FILE
