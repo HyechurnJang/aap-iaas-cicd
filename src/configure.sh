557d6f81ca7b4e4c7d68910ea0e5de789d088336
jangh <jangh@JZM5570>
2022-11-09 18:08:31 +0900

    update

--- src/configure.sh
+++ src/configure.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd /home/devops/aap-iaas-cicd
-git pull
+cd /home/devops/aap-iaas-cicd && git pull
 sudo rm -rf /var/www/html/index.html
 sudo cp /home/devops/aap-iaas-cicd/src/html/index.html /var/www/html/index.html
 sudo sed -i "s/__HOSTNAME__/$(hostname)/g" /var/www/html/index.html
