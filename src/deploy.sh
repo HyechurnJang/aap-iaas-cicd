ed24b487fdac50b9cae0a2825d795ebe3ae03a81
jangh <jangh@211.105.0.214>
2022-11-09 21:39:53 +0900

    update

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /home/devops/aap-iaas-cicd && git pull
+cp /home/devops/aap-iaas-cicd/src/html/index.html /tmp/index.html.tmp
 sudo rm -rf /var/www/html/index.html
 sudo cp /home/devops/aap-iaas-cicd/src/html/index.html /var/www/html/index.html
 sudo sed -i "s/__HOSTNAME__/$(hostname)/g" /var/www/html/index.html
